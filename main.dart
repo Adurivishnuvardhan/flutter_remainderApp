53173cd4256b0698d3e4d2c9b276010f3dd054be
ADURI VISHNU VARDHAN REDDY <86121713+Adurivishnuvardhan@users.noreply.github.com>
2024-08-08 20:04:36 +0530

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'reminder_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Reminder App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ReminderPage(),
+    );
+  }
+}
